87f8b94960f95a61b49e900afccf885f2b9f35ad
Javier Palacios <javiplx@gmail.com>
2012-07-23 12:10:39 +0200

    Ensure that serial terminal is enabled on modified boot images

--- do_bootfs.sh
+++ do_bootfs.sh
@@ -4,6 +4,8 @@ dd if=/dev/zero of=initrd.img bs=1M count=3
 mke2fs -F -b 1024 -i 1024 initrd.img 
 tune2fs -c 0 -i 0 initrd.img 
 
+echo 'ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100' >> bootfs/etc/inittab
+
 mkdir image 
 mount -o loop -t auto initrd.img image 
 tar cC bootfs . | tar xC image 
@@ -11,6 +13,8 @@ tar xC image -f bootfs-skel.tar
 umount image 
 rmdir image 
  
+sed -i -e '/^ttyS0/ d' bootfs/etc/inittab
+
 gzip -9 initrd.img 
 mv initrd.img.gz bootfs.bin
 
